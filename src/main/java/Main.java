9fe6f05adcf5e10f918fa86aed8b7c30dfb20423
Link <lenkgit@gmail.com>
2021-11-09 23:01:37 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import net.navibot.sdk.NaviPlugin;
+import net.navibot.sdk.data.Message;
+import net.navibot.sdk.data.Response;
+import org.pf4j.Extension;
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+public class Main extends Plugin {
+
+    public Main(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    /**
+     * root class that'll be processed for custom triggers,
+     * IT'S NOT RECOMMENDED YOU MOVE THIS CLASS, build around and in it
+     * but for best functionality - leave it in place!
+     */
+
+    @Extension
+    public static class MyPlugin implements NaviPlugin {
+
+        /**
+         * insert all of your code here for the incoming message invoking your trigger
+         * you can create http requests subject to review and many other fun stuff to respond with
+         * responses can be made up of both a card and a text if provided or one or the other
+         *
+         * @param message incoming message
+         * @return your response
+         */
+        public Response onMessage(Message message) {
+            return new Response(null, "world!");
+        }
+
+    }
+}
